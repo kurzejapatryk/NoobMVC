3b8a2d291f8a9492dd021343a9225adaae73cfa7
Patryk Kurzeja <patryk@kmpk.eu>
2024-07-09 06:41:37 +0200

    Update MySQL host to 'mysql' in Database.php

--- Configs/Database.php
+++ Configs/Database.php
@@ -6,10 +6,10 @@
 define('DB_type', 'MySQL');                     // Temporary only MySQL, integration of other databases later.
 
 $DB_CONF = [
-    'HOST' => '127.0.0.1',                      // Database host
+    'HOST' => 'mysql',                          // Database host
     'PORT' => '3306',                           // Database port
-    'USER' => 'root',                        // Database username
-    'PASSWORD' => '',                // Database password
+    'USER' => 'root',                           // Database username
+    'PASSWORD' => '',                           // Database password
     'DATABASE' => 'test_db',                    // Database name
     'DATETIME_FORMAT' => 'Y-m-d H:i:s'          // Date format in the database
 ];
