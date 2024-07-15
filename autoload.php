560e2b7dde161a004f4de3b4886aa5c912d58a43
Patryk Kurzeja <patryk@kmpk.eu>
2024-07-15 14:09:39 +0200

    Fix relative path in autload and bootstrap

--- autoload.php
+++ autoload.php
@@ -4,12 +4,12 @@
     $dir = scandir($conf_dir);
     foreach ($dir as $check_dir){
     if(is_file($conf_dir . $check_dir))
-        require_once($conf_dir . $check_dir);
+        require_once __DIR__ . '/'.$conf_dir . $check_dir;
     }
 
     // load classes
     spl_autoload_register(function ($name){
         $className = (string) str_replace('\\', DIRECTORY_SEPARATOR, $name);
-        require_once($className.'.class.php');
+        require_once __DIR__ . '/' . $className.'.class.php';
         return true;
     });
